356a43f6127ca2a39d8d5d1a77cf819c0165d619
Khôi Huỳnh <khoi.dn95@gmail.com>
2020-08-14 14:58:53 +0700

    Updates

---
+++ precache-manifest.0540234331603b9e5771c4c307d056c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98fea74414bf2a48672831e6a39ab7bf",
+    "url": "/random-quiz/index.html"
+  },
+  {
+    "revision": "a9a1b076268da49fc840",
+    "url": "/random-quiz/static/js/2.419901a3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/random-quiz/static/js/2.419901a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5901cab98ca83a0d1a17",
+    "url": "/random-quiz/static/js/main.16817045.chunk.js"
+  },
+  {
+    "revision": "a027f5de5fb696eeb562",
+    "url": "/random-quiz/static/js/runtime-main.1e7507d4.js"
+  },
+  {
+    "revision": "c11f97a3b321dad2ede69652d3f8a174",
+    "url": "/random-quiz/static/media/nattu-adnan-unsplash.c11f97a3.jpg"
+  }
+]);
